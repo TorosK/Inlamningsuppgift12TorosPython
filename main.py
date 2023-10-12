bf957423d77c5d4021bc6f015d3574d23ab7fe9c
TorosK <toros_kutlu@hotmail.com>
2023-10-12 10:05:48 +0000

    better structure

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ def get_integer_input(prompt, min_value, max_value):
         except ValueError:
             print("Ogiltig inmatning. Ange ett heltal.")
 
+def get_operation_input(prompt, valid_operations):
+    while True:
+        operation = input(prompt)
+        if operation in valid_operations:
+            return operation
+        else:
+            print("Ogiltigt räknesätt valt. Försök igen.")
+
 def generate_question(operation, table_or_divisor):
+    if operation not in ['*', '/', '%']:
+        raise ValueError("Invalid operation")
+
     factor_or_dividend = random.randint(1, 12)
     if operation == '*':
         return f"{factor_or_dividend} * {table_or_divisor}", factor_or_dividend * table_or_divisor
@@ -19,8 +30,6 @@ def generate_question(operation, table_or_divisor):
         return f"{factor_or_dividend * table_or_divisor} // {table_or_divisor}", factor_or_dividend
     elif operation == '%':
         return f"{factor_or_dividend} % {table_or_divisor}", factor_or_dividend % table_or_divisor
-    else:
-        return None
 
 def choose_door(num_doors):
     zombie_door = random.randint(1, num_doors)
@@ -32,66 +41,60 @@ def choose_door(num_doors):
         print(f"Säker! Zombiesarna var bakom dörr {zombie_door}.")
         return True
 
-# Variabler för att spara spelets förutsättningar
-previous_num_questions = None
-previous_operation = None
-previous_table_or_divisor = None
-user_won_last_game = False
-
-# Variabel för att hålla reda på ställda frågor
-asked_questions = {}
-
-continue_game = True
-while continue_game:
-    # Nollställ variabler för ny spelomgång
-    correct_answers = 0
-    questions_asked = 0
-    asked_questions = {}
-    
-    if user_won_last_game or previous_num_questions is None:
+def setup_game_conditions(won_last_game, previous_conditions):
+    if won_last_game or previous_conditions['num_questions'] is None:
         num_questions = get_integer_input("Välj antal frågor (12 - 39): ", 12, 39)
-        operation = input("Välj räknesätt (*, /, %, slump): ")
-        if operation not in ['*', '/', '%', 'slump']:
-            print("Ogiltigt räknesätt valt. Försök igen.")
-            continue
+        operation = get_operation_input("Välj räknesätt (*, /, %, slump): ", ['*', '/', '%', 'slump'])
     else:
-        num_questions = previous_num_questions
-        operation = previous_operation
+        num_questions = previous_conditions['num_questions']
+        operation = previous_conditions['operation']
 
     if operation != 'slump':
-        if user_won_last_game or previous_table_or_divisor is None:
+        if won_last_game or previous_conditions['table_or_divisor'] is None:
             if operation == '*':
                 table_or_divisor = get_integer_input("Välj en tabell (2 - 12): ", 2, 12)
             elif operation in ['/', '%']:
                 table_or_divisor = get_integer_input("Välj en divisor (2 - 5): ", 2, 5)
         else:
-            table_or_divisor = previous_table_or_divisor
+            table_or_divisor = previous_conditions['table_or_divisor']
     else:
         table_or_divisor = None
 
-    # Spara spelets förutsättningar
-    previous_num_questions = num_questions
-    previous_operation = operation
-    previous_table_or_divisor = table_or_divisor
+    return num_questions, operation, table_or_divisor
 
-    # Reset variables
-    questions_asked = 0
+# Initialize game variables
+previous_conditions = {'num_questions': None, 'operation': None, 'table_or_divisor': None}
+user_won_last_game = False
+
+continue_game = True
+while continue_game:
     correct_answers = 0
+    questions_asked = 0
+    asked_questions = {}
+
+    num_questions, operation, table_or_divisor = setup_game_conditions(user_won_last_game, previous_conditions)
+
+    previous_conditions['num_questions'] = num_questions
+    previous_conditions['operation'] = operation
+    previous_conditions['table_or_divisor'] = table_or_divisor
+
     user_won_last_game = False  # Reset flag
 
     while questions_asked < num_questions:
-        # Generera en unik fråga
         # Inre loop för att hantera varje fråga
         question_loop = True
         while question_loop:
             current_operation = operation if operation != 'slump' else random.choice(['*', '/', '%'])
             current_table_or_divisor = random.randint(2, 12 if current_operation == '*' else 5) if operation == 'slump' else table_or_divisor
-            question, answer = generate_question(current_operation, current_table_or_divisor)
+            
+            try:
+                question, answer = generate_question(current_operation, current_table_or_divisor)
+            except ValueError:
+                print("Invalid operation, please restart the game.")
+                break
 
-            # Kolla om frågan redan har ställts och hur många gånger
             times_asked = asked_questions.get(question, 0)
 
-            # Regler för att förhindra repetering
             if num_questions <= 13 and times_asked == 0:
                 break
             elif 14 <= num_questions <= 26 and times_asked < 2:
@@ -99,7 +102,6 @@ while continue_game:
             elif 27 <= num_questions <= 39 and times_asked < 3:
                 break
 
-        # Uppdatera antalet gånger frågan har ställts
         asked_questions[question] = asked_questions.get(question, 0) + 1
 
         print(f"Du har besvarat {questions_asked} frågor korrekt av {num_questions} möjliga.")
@@ -119,7 +121,7 @@ while continue_game:
         questions_asked += 1
 
         print(f"Du har nu {correct_answers} korrekta svar och {num_questions - questions_asked} frågor kvar.")
-        
+
         if questions_asked == num_questions:
             print("Grattis! Du har vunnit!")
             user_won_last_game = True
